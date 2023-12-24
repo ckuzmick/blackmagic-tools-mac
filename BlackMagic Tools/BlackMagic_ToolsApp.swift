046e0337c5e140a1f0101f4a6679519f52a9431a
ckuzmick <conrad.kuzmick@icloud.com>
2023-12-24 11:09:45 -0500

    Initial Commit

---
+++ BlackMagic Tools/BlackMagic_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackMagic_ToolsApp.swift
+//  BlackMagic Tools
+//
+//  Created by ck on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct BlackMagic_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
